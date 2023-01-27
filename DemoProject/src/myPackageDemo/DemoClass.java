a75c6d80b88a4359dab38b2a9db72f576a48f648
Santu08 <123650580+Santu08@users.noreply.github.com>
2023-01-27 21:46:56 +0530

    BasicTask

---
+++ DemoProject/src/myPackageDemo/DemoClass.java
@@ -0,0 +1,18 @@
+package myPackageDemo;
+
+public class DemoClass {
+	
+	public static void main(String args[])
+	
+	{
+		
+		
+		System.out.println("TestBase");
+		System.out.println("Pom.xml");
+		System.out.println("Utilities");
+		System.out.println("Resource");
+		System.out.println("Suite");
+	
+	}
+
+}
